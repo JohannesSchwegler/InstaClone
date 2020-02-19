6d9333af4f42feb03bf6085df84a5fb04949a9f4
Johannes Schwegler <johannes.schwegler@outlook.de>
2020-02-19 22:01:10 +0100

    first commit

---
+++ feed/static/js/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("load",()=>{
+
+let sidebar = document.querySelector('.sidebar');
+
+sidebar.style.left=window.innerWidth-495+"px";
+
+window.addEventListener("resize",()=>{
+    let width= window.innerWidth;
+   
+})
+  
+
+
+
+
+
+
+
+
+})
